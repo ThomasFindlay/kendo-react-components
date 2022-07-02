869179aa931bc111eee6569729a237484442a52e
Thomas <thomasfindlay94@gmail.com>
2022-07-02 16:42:20 +0100

    datepicker

---
+++ javascript-with-kendo-react-components/src/components/MyDatePicker.jsx
@@ -0,0 +1,7 @@
+import { DatePicker } from "@progress/kendo-react-dateinputs";
+
+const MyDatePicker = props => {
+  return <DatePicker format="MM-dd-yyyy" {...props} />;
+};
+
+export default MyDatePicker;
